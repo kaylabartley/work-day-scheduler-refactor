f2d4ac68554a2875ed9262fceb640c01491fc60e
Green S <kaylasierrabartley@gmail.com>
2021-04-18 19:06:59 -0500

    got past, present, and future to work

--- script.js
+++ script.js
@@ -15,7 +15,62 @@ THEN the saved events persist
 
 //$("#currentDay").html(date);
 
-var options = {  weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+var options = {  weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
 var prnDt =  new Date().toLocaleTimeString('en-us', options);
 
 $("#currentDay").html(prnDt);
+ var todoText = [{}, {}, {}, {}, {}, {}, {}, {}, {}];
+
+
+
+var loadItems = function() {
+    var inputs = document.getElementsByTagName("textarea") 
+    var hour =  new Date().toLocaleTimeString(navigator.language, {
+        hour: '2-digit',
+        hour12: false 
+    });
+
+   var hour= parseInt(hour);
+   
+    for(var i=0; i<inputs.length; i++ ){
+        var input = inputs[i];
+        console.log(hour + " "+input.id);
+        if (hour>input.id){
+            input.classList.add("past");
+        }
+        if(hour<input.id){
+            input.classList.add("future");
+        }
+        if(hour==input.id){
+            input.classList.add("present");
+        }
+        
+        
+        
+    }
+    todoText = JSON.parse(localStorage.getItem("text"));
+  
+    // if nothing in localStorage, create a new object to track all task status arrays
+    if (!tasks) {
+      tasks = {
+        toDo: [],
+        inProgress: [],
+        inReview: [],
+        done: []
+      };
+    }
+  
+    // loop over object properties
+    $.each(tasks, function(list, arr) {
+      // then loop over sub-array
+      arr.forEach(function(task) {
+        createTask(task.text, task.date, list);
+      });
+    });
+  };
+
+  var saveItems = function() {
+    localStorage.setItem("text", JSON.stringify(tasks));
+  };
+
+  loadItems();
